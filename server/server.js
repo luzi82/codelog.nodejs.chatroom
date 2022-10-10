c0acab30218437ec23d6c9f3e6b3220a8c945827
luzi82 <luzi82@gmail.com>
2022-10-10 16:46:20 +0800

    simple server base

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const SocketServer = require('ws').Server
+
+const PORT = 3000
+
+const server = express()
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+const wss = new SocketServer({ server })
+
+wss.on('connection', ws => {
+
+    console.log('Client connected')
+
+    ws.on('close', () => {
+        console.log('Close connected')
+    })
+})
