95479bb2ea60d9be99ad6655523a7a50f231c1d8
luzi82 <luzi82@gmail.com>
2022-10-10 21:33:52 +0800

    progress

---
+++ client/src/ChatClient.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    Hello World
+    <button @click="asdfOnClick">ASDF</button>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    let ws = new WebSocket('ws://localhost:3000');
+    return {
+      ws: ws,
+    };
+  },
+  methods: {
+    asdfOnClick(event) {
+      console.log('asdf');
+    },
+  },
+}
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import ChatClient from './ChatClient.vue'
 
-import './assets/main.css'
+// import './assets/main.css'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+createApp(ChatClient).mount('#app')
